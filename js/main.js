48f375d7b4e4ba1dce4f3840ff07ccc0d6d7016b
Habibur Rahman <habibur88454@gmail.com>
2024-09-24 17:53:50 +0600

    initial setup done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const milestonesData = JSON.parse(data).data
+console.log(milestonesData)
+
+function loadMilestones() {
+    const milestones = document.querySelector('.milestones');
+
+    //  milestones.innerHTML = `
+
+    //  `
+}
+
+loadMilestones();
