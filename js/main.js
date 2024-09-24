91a8ae309697e5ee42fc69facc360daa6a592d1c
Habibur Rahman <habibur88454@gmail.com>
2024-09-24 22:16:08 +0600

    added milestones

--- js/main.js
+++ js/main.js
@@ -4,9 +4,29 @@ console.log(milestonesData)
 function loadMilestones() {
     const milestones = document.querySelector('.milestones');
 
-    //  milestones.innerHTML = `
+     milestones.innerHTML = ` ${milestonesData.map(function(milestone){
+       return `
+        <div class="milestone border-b">
+            <div class="flex">
+              <div class="checkbox"><input type="checkbox" /></div>
+              <div>
+                <p>
+                 ${milestone.name}
+                  <span><i class="fas fa-chevron-down"></i></span>
+                </p>
+              </div>
+            </div>
+            <div class="hidden_panel">
+              <div class="module border-b">
+                <p>Module Name</p>
+              </div>
+            </div>
+          </div>
+       
+       `
+     }).join('')}
 
-    //  `
+     `
 }
 
 loadMilestones();
